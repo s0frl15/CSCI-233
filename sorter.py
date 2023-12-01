2997cab3e3f0185af3cb265536e921d3c8f14975
paxleslie <128331821+paxleslie@users.noreply.github.com>
2023-12-01 12:11:10 -0600

    Create sorter.py
    final sorter

---
+++ sorter.py
@@ -0,0 +1,43 @@
+
+
+from ticketclass import Ticket_List  
+def highest_ticket():
+
+    ticket_list = Ticket_List()
+
+    customer_list = []
+    developer_list = []
+    individual_list = []
+
+    for ticket in ticket_list:  # sorts based on impact
+        if ticket.impact == "customer":
+            customer_list.append(ticket)
+            customer_list.sort(key=lambda ticket: ticket.date_time)
+        elif ticket.impact == "developer":
+            developer_list.append(ticket)
+            developer_list.sort(key=lambda ticket: ticket.date_time)
+        else:
+            individual_list.append(ticket)
+            individual_list.sort(key=lambda ticket: ticket.date_time)
+
+    #combines the 3 list into 1
+    sorted_list = customer_list + developer_list + individual_list
+
+    # TESTING ONLEY LINE 26-37, 43
+    #print("\nMerged and Sorted List:")
+    #for ticket in sorted_list:
+    #    print(f"{ticket.emp_name}")  #just prints the names in order for testing
+
+    #desired output
+    #alice
+    #john
+    #pax
+    #bob
+    #jesus
+    #levi
+
+
+    return sorted_list[0]
+
+# TESTING ONLEY
+#highest_ticket()
